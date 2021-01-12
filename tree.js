3792a03424a88e40c0480c50a89c04aa62bab695
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2021-01-13 04:13:45 +0530

    Improved tree design.

--- tree.js
+++ tree.js
@@ -17,6 +17,8 @@ class Tree {
         rectMode(CENTER);
         fill(181, 101, 29); //brown for tree trunk
         rect(this.x, this.y + 25, 15, 100);
+        fill(140, 70, 29);
+        rect(this.x + 7, this.y + 25, 5, 100);
         fill(0, this.treeColour, 0);
         triangle (this.x, this.y + 50, this.x, this.y - 75, this.x - 40, this.y + 50);
         fill(0, this.treeColour - 30, 0);
